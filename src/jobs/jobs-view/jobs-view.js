875730bfc23f8ccedd623817ac894284613a61eb
Nikita Zverev <nikita.zverev@konverso.ai>
2019-03-20 16:30:36 +0500

    Added actions to the jobs-vew component

--- src/jobs/jobs-view/jobs-view.js
+++ src/jobs/jobs-view/jobs-view.js
@@ -6,7 +6,8 @@ import './jobs-view.scss';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardHeader from '@material-ui/core/CardHeader';
-
+import CardActions from '@material-ui/core/CardActions';
+import Button from '@material-ui/core/Button';
 
 export class JobsView extends Component {
     lMap;
@@ -17,6 +18,9 @@ export class JobsView extends Component {
 
         this.initMap = this.initMap.bind(this);
         this.addMarker = this.addMarker.bind(this);
+        this.jobApply = this.jobApply.bind(this);
+        this.jobSave = this.jobSave.bind(this);
+        this.jobDelete = this.jobDelete.bind(this);
     }
 
     componentDidMount(){
@@ -55,6 +59,24 @@ export class JobsView extends Component {
         this.lMarker.setLatLng(latLng);
     }
 
+    jobApply() {
+        if(typeof this.props.onJobApply === 'function'){
+            this.props.onJobApply(this.props.job_title);
+        }
+    }
+
+    jobSave() {
+        if(typeof this.props.onJobSave === 'function'){
+            this.props.onJobSave(this.props.job_title);
+        }
+    }
+
+    jobDelete() {
+        if(typeof this.props.onJobDelete === 'function'){
+            this.props.onJobDelete(this.props.job_title);
+        }
+    }
+
     render(){
         return (
             <Card className="jobs-view">
@@ -66,6 +88,20 @@ export class JobsView extends Component {
                 <CardContent>
                     <div id="leafletMap" className="job-view__map"></div>
                 </CardContent>
+
+                <CardActions 
+                    className="jobs-vew-actions">
+                    <div 
+                        className="jobs-view-actions--positive">
+                        <Button 
+                            variant="outlined"
+                            color="primary">Apply</Button>                    
+                        <Button>Save</Button>
+                    </div>
+                    <Button 
+                        color="secondary"
+                        onClick={this.jobDelete}>Delete</Button>                    
+                </CardActions>
             </Card>
         ); 
     }
